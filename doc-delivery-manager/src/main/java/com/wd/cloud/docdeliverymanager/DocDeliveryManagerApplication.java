7088c413baeea2003fcb69c173f05d1efe53d750
何志刚 <hezhigang@hnwdkj.com>
2018-06-14 18:29:03 +0800

    代码规范

---
+++ doc-delivery-manager/src/main/java/com/wd/cloud/docdeliverymanager/DocDeliveryManagerApplication.java
@@ -0,0 +1,12 @@
+package com.wd.cloud.docdeliverymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocDeliveryManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocDeliveryManagerApplication.class, args);
+    }
+}

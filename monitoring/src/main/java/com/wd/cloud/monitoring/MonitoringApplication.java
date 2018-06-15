fbd595083600c385790e85bba60d9e822bf64374
shenfu <shenfu@hnwdkj.com>
2018-06-15 11:22:05 +0800

    监控服务

---
+++ monitoring/src/main/java/com/wd/cloud/monitoring/MonitoringApplication.java
@@ -0,0 +1,68 @@
+package com.wd.cloud.monitoring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import de.codecentric.boot.admin.server.domain.entities.InstanceRepository;
+import de.codecentric.boot.admin.server.notify.Notifier;
+import de.codecentric.boot.admin.server.notify.RemindingNotifier;
+
+@Configuration
+@EnableAutoConfiguration
+@SpringBootApplication
+@EnableHystrixDashboard
+@EnableDiscoveryClient
+@EnableTurbine
+@EnableAdminServer
+public class MonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringApplication.class, args);
+	}
+	
+	@Configuration
+	public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+		@Override
+	    protected void configure(HttpSecurity http) throws Exception {
+	    	http.authorizeRequests().anyRequest().permitAll()  
+	                .and().csrf().disable();
+		}
+	}
+	
+	/**
+	 * SBA通知
+	 * @author Administrator
+	 *
+	 */
+	@Configuration
+	@EnableScheduling
+	public class NotifierConfiguration {
+	    @Autowired
+	    private Notifier notifier;
+	    
+	    @Autowired
+	    private InstanceRepository repository;
+
+	    @Bean
+	    @Primary
+	    public RemindingNotifier remindingNotifier() {
+	        RemindingNotifier remindingNotifier = new RemindingNotifier(notifier, repository);
+	        //remindingNotifier.setReminderPeriod(Duration.ofMinutes(5));
+	        remindingNotifier.setReminderStatuses(new String[]{"DOWN"}); //服务下线
+	        return remindingNotifier;
+	    }
+	   
+	}
+}

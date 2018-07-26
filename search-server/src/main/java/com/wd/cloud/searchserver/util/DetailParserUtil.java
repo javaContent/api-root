36a2d6ab34d2b178e4de82afb92470f526eeb18c
shenfu <shenfu@hnwdkj.com>
2018-07-26 10:23:47 +0800

    部分代码修改。

--- search-server/src/main/java/com/wd/cloud/searchserver/util/DetailParserUtil.java
+++ search-server/src/main/java/com/wd/cloud/searchserver/util/DetailParserUtil.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.omg.CORBA.SystemException;
 
 import com.wd.cloud.searchserver.entity.AuthorityDatabase;

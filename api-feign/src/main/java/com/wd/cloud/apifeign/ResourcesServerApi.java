6ea2b7b4b2d6d5374383223e8a11d82cb68bfa2e
何志刚 <hezhigang@hnwdkj.com>
2018-08-01 16:14:24 +0800

    feign调用文件上传接口

--- api-feign/src/main/java/com/wd/cloud/apifeign/ResourcesServerApi.java
+++ api-feign/src/main/java/com/wd/cloud/apifeign/ResourcesServerApi.java
@@ -1,19 +1,47 @@
 package com.wd.cloud.apifeign;
 
+import cn.hutool.json.JSONObject;
 import com.wd.cloud.commons.model.ResponseModel;
-import com.wd.cloud.commons.vo.UserVo;
+import feign.codec.Encoder;
+import feign.form.spring.SpringFormEncoder;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
+
+
 
 /**
  * @author He Zhigang
  * @date 2018/7/24
  * @Description:
  */
-@FeignClient(value = "resources-server", fallback=AuthServerApi.HystrixCalculatorService.class)
+@FeignClient(value = "resources-server",configuration = ResourcesServerApi.MultipartSupportConfig.class)
 public interface ResourcesServerApi {
 
-    @PostMapping("/upload/doc")
-    public ResponseModel uploadDocDeliveryFile(MultipartFile file);
+    @PostMapping(value = "/upload/{dir}",consumes = "multipart/form-data")
+    public ResponseModel<JSONObject> uploadCustomer(@RequestPart(value = "file") MultipartFile file,
+                                                    @PathVariable String dir);
+
+    @PostMapping(value = "/upload/doc",consumes = "multipart/form-data")
+    public ResponseModel<JSONObject> uploadDocDeliveryFile(@RequestPart(value = "file") MultipartFile file);
+
+    @PostMapping(value = "/upload/image",consumes = "multipart/form-data")
+    public ResponseModel<JSONObject> uploadImageFile(@RequestPart(value = "file") MultipartFile file);
+
+    @PostMapping(value = "/upload/journal/{journalId}",consumes = "multipart/form-data")
+    public ResponseModel<JSONObject> uploadJournalImageFile(@RequestPart(value = "file") MultipartFile file,
+                                                            @PathVariable String journalId);
+
+
+
+
+    class MultipartSupportConfig {
+        @Bean
+        public Encoder feignFormEncoder() {
+            return new SpringFormEncoder();
+        }
+    }
 }
